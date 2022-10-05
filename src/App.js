87bdf7b4d0c9bce927f225390ffbf3b71738bf67
Stephanie Estrada Garcia <stephanieestradagarcia@stephanies-air.home>
2022-10-05 22:29:04 +0100

    All component folders were created and imported into 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Contact from './components/Contact/Contact'
+import Portfolio from './components/Portfolio/Portfolio'
+import Experience from './components/Experience/Experience'
 
 function App() {
   return (   
+  <>
+  <Header/>
+  <Nav/>
+  <About/>
+  <Footer/>
+  <Contact/>
+  <Portfolio/>
+  <Experience/>
+  </>
   );
 }
 
