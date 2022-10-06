7af83e118f7d0c49c0982158d541901afb3937aa
Stephanie Estrada Garcia <stephanieestradagarcia@stephanies-air.home>
2022-10-06 21:22:47 +0100

    Created css variables and added general css styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
-import Nav from './components/Nav/Nav'
-import About from './components/About/About'
-import Footer from './components/Footer/Footer'
-import Contact from './components/Contact/Contact'
-import Portfolio from './components/Portfolio/Portfolio'
-import Experience from './components/Experience/Experience'
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
+import Experience from './components/Experience/Experience';
 
 function App() {
   return (   
   <>
-  <Header/>
-  <Nav/>
-  <About/>
-  <Footer/>
-  <Contact/>
-  <Portfolio/>
-  <Experience/>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Footer/>
+      <Contact/>
+      <Portfolio/>
+      <Experience/>
   </>
   );
 }
